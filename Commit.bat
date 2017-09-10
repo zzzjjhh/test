a7817b87539d4ad5b6d63de49cba5eee0b40665a
zzzjjhh <84010412@qq.com>
2017-09-10 10:00:48 +0800

    by Dgbc-Zjh-X230i ip 172.17.1.145 at 2017/09/10 周日

--- Commit.bat
+++ Commit.bat
@@ -1,15 +1,9 @@
-:: git的全局配置
-
-git config --global user.name "zzzjjhh"
-git config --global user.email 84010412@qq.com
-
-::支持中文路径
-git config --global core.quotepath false
+call Config.bat
 
 ::本地提交
-cd /d "e:/git/Swan"
+
 git add -A
-git commit -a -m "by Dgbc-Zjh-X230i ip 172.17.1.136"
+git commit -a -m "by Dgbc-Zjh-X230i ip 172.17.1.145 at %date%"
 
 ::远程提交
 git push -u origin master
